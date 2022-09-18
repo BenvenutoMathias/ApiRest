13d028e05f32b2c18ed02b623c4a0e3e1edbf45e
BenvenutoMathias <benvenutomathias65@gmail.com>
2022-09-18 20:35:59 -0300

    Proyecto Base

---
+++ Dtos/CreateItemDto.cs
@@ -0,0 +1,14 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RestApi.Dtos
+{
+    public record CreateItemDto
+    {   
+        [Required]
+        public string Name { get; init; }
+        [Required]
+        [Range(1, 1000)]
+        public decimal Price { get; init; }
+    }
+}
---
+++ Extensions.cs
@@ -0,0 +1,20 @@
+using RestApi.Entities;
+using RestApi.Dtos;
+
+namespace RestApi
+{
+    public static class Extensions
+    {
+        public static ItemDto AsDto(this Item item)
+        {
+            return new ItemDto
+            {
+                Id = item.Id,
+                Name = item.Name,
+                Price = item.Price,
+                CreatedDate = item.CreatedDate
+            };
+        }
+    }
+
+}
